e2201cc1a764bf9da67d09ee7eb05277cd5f3d3c
Buchiiii <mokwunyebuchi@gmail.com>
2024-02-19 10:31:21 +0100

    cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
 const corOptions = {
-  origin: process.env.ORIGIN_URL,
+  origin: "*",
   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
   credentials: true,
   optionSucessStatus: 200,
