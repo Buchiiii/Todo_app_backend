2bce52e90da7affd7c3bc79be1c9cfcbf13a0ab1
Buchiiii <mokwunyebuchi@gmail.com>
2024-02-19 10:26:27 +0100

    cors

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
 const corOptions = {
-  origin: process.env.ORIGIN_URL,
+  origin: "*",
   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
   credentials: true,
   optionSucessStatus: 200,
 };
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 app.use(cors(corOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 
