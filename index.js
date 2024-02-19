8d9b8a3e46249ba3ab16e15c92a81120cfeac570
Buchiiii <mokwunyebuchi@gmail.com>
2024-02-19 14:00:38 +0100

    refactoring

--- index.js
+++ index.js
@@ -7,19 +7,13 @@ const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
 const corOptions = {
-  origin: "*",
+  origin: process.env.ORIGIN_URL,
   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
   credentials: true,
   optionSucessStatus: 200,
 };
 app.use(express.json());
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// });
-app.use(cors());
+app.use(cors(corOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = mysql.createPool({
